81fc6ed455c535d26a78943de129189e5d1e8404
Max Lee <maxolee23@gmail.com>
2019-06-09 17:17:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
   return object;
 }
 
-function deleteFromObjectByKey (object, key) {
-  var newObject = Object.assign({}, object);
-  delete newObject.key;
-  return newObject;
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj.key;
+  return newObj;
 }
 
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
+}
